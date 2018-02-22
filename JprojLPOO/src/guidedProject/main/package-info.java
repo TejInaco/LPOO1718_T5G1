cbd140731824f1f234aea24838c023a61eed9f10
luis <luis@linuxAcer>
2018-02-22 23:57:35 +0000

    added new function

--- JprojLPOO/src/guidedProject/main/package-info.java
+++ JprojLPOO/src/guidedProject/main/package-info.java
@@ -1,7 +1,7 @@
 
 /**
  * @author Ricardo Silva up201607780
- * @author Luís Oliveira
+ * @author Luís Oliveira up201607946
  * {@link} https://github.com/twitter/commons/blob/master/src/java/com/twitter/common/styleguide.md
  * @
  */
