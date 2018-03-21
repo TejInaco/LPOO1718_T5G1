6cdf09ba77e412e8bbe65bb15316bbd803f7409b
Ricardo MG Silva <up201607780@fe.up.pt>
2018-03-21 16:30:04 +0000

    Confg Corrected v2

---
+++ src/logic/Hero.java
@@ -0,0 +1,10 @@
+package logic;
+
+public class Hero extends BoardObject {
+
+	public Hero(int line, int col) {
+		super(line, col, 'H');
+}
+
+}
+ 
---
+++ src/logic/Level.java
@@ -0,0 +1,116 @@
+package logic;
+
+public class Level {
+	protected char[][] map;
+	public boolean isDoorsOpen;
+	
+	public Level(int numb) {
+		if(numb == 1) {
+			char [][] mapaToUse =  { 
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+					{ 'X', ' ', ' ', ' ', 'I', ' ', 'X', ' ', ' ', 'X' }, 
+					{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+					{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X' }, 
+					{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+					{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X' }, 
+					{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', 'k', ' ', 'X' },
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
+			this.map = mapaToUse;
+			
+		}
+		if(numb == 2) {
+			char [][] mapaToUse = { 
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+					{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', 'k', 'X' }, 
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }, };
+			this.map = mapaToUse;
+
+		}
+		if(numb == 3) { //varios guardas
+			char [][] mapaToUse =  { 
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+					{ 'X', ' ', ' ', ' ', 'I', ' ', 'X', ' ', ' ', 'X' }, 
+					{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+					{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X' }, 
+					{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+					{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X' }, 
+					{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', 'k', ' ', 'X' },
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
+			this.map = mapaToUse;
+	
+		}
+		if(numb == 4) { //varios Ogres
+			char [][] mapaToUse = { 
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+					{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', 'k', 'X' }, 
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+					{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+					{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }, };
+			this.map = mapaToUse;
+	
+		}
+		this.isDoorsOpen =false;
+	}
+	
+	public Level(char[][] mp) {
+		this.map = mp;
+		this.isDoorsOpen =false;
+	}
+	
+	public char[][] getMap() {
+		return this.map;
+	}
+	public boolean getisDoorsOpen() {
+		return this.isDoorsOpen;
+	}
+	public void setMap(char[][] mp) {
+		this.map = mp;
+	}
+	public boolean isEmpty(int lin, int col) {
+		if (this.map[lin][col] == 'X' || this.map[lin][col] == 'I' || lin < 0 || lin >= 10 || col < 0 || col >= 10)
+			return false;
+		else 
+			return true;
+	}
+
+	public boolean gotKey(int lin, int col) {
+		
+		if (this.map[lin][col] == 'k')
+			return true;
+		else
+			return false;
+	}
+
+	public void openDoors() {
+		for (int i = 0; i < this.map.length; i++) {
+			if (this.map[i][0] == 'I')
+				this.map[i][0] = 'S';
+				this.isDoorsOpen = true;
+		}
+	}
+
+	public boolean foundDoor(int lin, int col) {
+		if (this.map[lin][col] == 'S')
+			return true;
+		else
+			return false;
+	}
+
+	
+
+	
+}
