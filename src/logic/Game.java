53d55935b0dd06c0b016eb4a03720eddf85aaf20
Ricardo MG Silva <up201607780@fe.up.pt>
2018-03-21 22:53:20 +0000

    Ogre Movement working
    CrazyOgre Movement running, refactoring vallideRules()

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -25,6 +25,8 @@ public class Game {
 		this.hero = new Hero(1, 1);
 		this.crazyOgre = new Ogre();
 	}
+	
+	
 	public Game(int ogres, Object tipo) {
 		this.gameover = false;
 		this.ended = false;
@@ -61,8 +63,7 @@ public class Game {
 	}
 	public void setCrazyOgre(Ogre m) {
 		this.crazyOgre  = m;
-	}
-	
+	}	
 	public void setLevel(char[][] lvl) {
 		 this.board = new Level(lvl);
 	}
@@ -94,14 +95,11 @@ public class Game {
 	public void setHero(Hero heroi) {
 		this.hero = heroi;
 	}
-	
 	public void setGameOver(boolean bln) {
 		this.gameover = bln;
 		
 	}
-	
 	public void setGuard() {
-
 		Random rand = new Random();
 
 		int random = rand.nextInt(3);
@@ -120,9 +118,6 @@ public class Game {
 			break;
 		}
 	}
-
-	
-	
 	// TENTAR COLOCAR EM OGRE
 	public void setOgre() {
 
@@ -141,14 +136,14 @@ public class Game {
 	public void move(BoardObject foe) {
 
 		if (foe instanceof Guard)
-			((Guard) foe).move();
+			this.getGuard().move();
 
 		else if (foe instanceof Ogre) {
 
 			while (true) {
 
-				int newLine = ((Ogre) foe).getLine();
-				int newCol = ((Ogre) foe).getCol();
+				int newLine = this.crazyOgre.getLine();
+				int newCol = this.crazyOgre.getCol();
 
 				Random random = new Random();
 
@@ -170,8 +165,8 @@ public class Game {
 				}
 
 				if (board.isEmpty(newLine, newCol) && !board.foundDoor(newLine, newCol)) {
-					((Ogre) foe).setLine(newLine);
-					((Ogre) foe).setCol(newCol);
+					this.crazyOgre.setLine(newLine);
+					this.crazyOgre.setCol(newCol);
 					break;
 				}
 			}
@@ -243,153 +238,131 @@ public class Game {
 	public void validateRules() {
 
 		if (this.level == 1) {  //heroi e um guarda apenas
-
-			this.guard.move();
-
-			if (this.board.gotKey(this.hero.getLine(), this.hero.getCol()))
-				this.board.openDoors();
-
-			if (this.guard.collision(this.hero.getLine(), this.hero.getCol()))
-				this.gameover = true;
-
-			if (this.board.foundDoor(this.hero.getLine(), this.hero.getCol()))
-				this.passed = true;
+			this.validateRulesLevel1();
 		}
-//////////		
 		if (this.level == 2) {//heroi e um ogre com club
-	
-			if (this.getCrazyOgre().getStun()) {
-					move(this.getCrazyOgre());
-			}else{
-				this.getCrazyOgre().incStunCounter();
-			}
-			
-			CastClub(this.getCrazyOgre());
-			
-			//check if hero tem a chave
-			if (this.board.gotKey(this.hero.getLine(), this.hero.getCol())) {
-				this.board.openDoors();
-				this.hero.setSymbol('K');
-			}
-			//
-			
-			if (this.getCrazyOgre().collision(this.hero.getLine(), this.hero.getCol()))
-					this.setGameOver(true);
-
-			if (this.crazyOgre.stun(this.hero.getLine(), this.hero.getCol())) {
-				this.crazyOgre.setSymbol('8');
-				this.crazyOgre.setStun();
-			}
-			
-			if (this.board.gotKey(this.crazyOgre.getCol(), this.crazyOgre.getLine())) {
-				this.crazyOgre.setSymbol('$');
-			}else if (!this.crazyOgre.getStun()) {
-				this.crazyOgre.setSymbol('O');
-			}
-
-			if (this.board.gotKey(this.crazyOgre.getClub().getCol(), this.crazyOgre.getClub().getLine()))
-				this.crazyOgre.getClub().setSymbol('$');
-			else
-				this.crazyOgre.getClub().setSymbol('*');
-
-			
-			if (this.board.foundDoor(this.hero.getLine(), this.hero.getCol()))
-				this.ended = true;
-		}
-
+			this.validateRulesLevel2();
+		}	
 		if (this.level == 3) {// heroi e 3 guardas
-			//TODO rever o codigo para os 3 guardas
-			this.guard.move();
-
-			if (this.board.gotKey(this.hero.getLine(), this.hero.getCol()))
-				this.board.openDoors();
-
-			if (this.guard.collision(this.hero.getLine(), this.hero.getCol()))
-				this.setGameOver(true);
-
-			if (board.foundDoor(hero.getLine(), hero.getCol()))
-				passed = true;
-
+			validateRulesLevel3();
 		}
 		if (this.level == 4) {//heroi e muitos Ogres
-
-			for (int i = 0; i < ogres.length; i++) {
-
-				if (!ogres[i].getStun())
-					move(ogres[i]);
-				else
-					ogres[i].incStunCounter();
-
-				CastClub(ogres[i]);
-
-			}
-
-			if (board.gotKey(hero.getLine(), hero.getCol())) {
-				board.openDoors();
-				hero.setSymbol('K');
-			}
-
-			for (int i = 0; i < ogres.length; i++) {
-
-				if (ogres[i].collision(hero.getLine(), hero.getCol()))
-					gameover = true;
-
-				if (ogres[i].stun(hero.getLine(), hero.getCol())) {
-					ogres[i].setSymbol('8');
-					ogres[i].setStun();
-				}
-
-				if (board.gotKey(ogres[i].getCol(), ogres[i].getLine()))
-					ogres[i].setSymbol('$');
-				else if (!ogres[i].getStun())
-					ogres[i].setSymbol('O');
-
-				if (board.gotKey(ogres[i].getClub().getCol(), ogres[i].getClub().getLine()))
-					ogres[i].getClub().setSymbol('$');
-				else
-					ogres[i].getClub().setSymbol('*');
-
-			}
-
-			if (board.foundDoor(hero.getLine(), hero.getCol()))
-				ended = true;
+			validateRulesLevel4();
 		}
+	}
+	
+	public void validateRulesLevel1() {
+		this.guard.move();
 
+		if (this.board.gotKey(this.hero.getLine(), this.hero.getCol()))
+			this.board.openDoors();
+
+		if (this.guard.collision(this.hero.getLine(), this.hero.getCol()))
+			this.gameover = true;
+
+		if (this.board.foundDoor(this.hero.getLine(), this.hero.getCol()))
+			this.passed = true;		
+	}
+	
+	public void validateRulesLevel2() {
+		//TODO verificar o comportamento do Ogre com o Luis
+		move(this.getCrazyOgre());
+		
+		if (this.getCrazyOgre().getStun()) {
+			move(this.getCrazyOgre());
+		}else{
+			this.getCrazyOgre().incStunCounter();
+		}
+	
+	CastClub(this.getCrazyOgre());
+	
+	//check if hero tem a chave
+	if (this.board.gotKey(this.hero.getLine(), this.hero.getCol())) {
+		this.board.openDoors();
+		this.hero.setSymbol('K');
+	}
+	
+	if (this.getCrazyOgre().collision(this.hero.getLine(), this.hero.getCol()))
+			this.setGameOver(true);
+
+	if (this.crazyOgre.stun(this.hero.getLine(), this.hero.getCol())) {
+		this.crazyOgre.setSymbol('8');
+		this.crazyOgre.setStun();
+	}
+	
+	if (this.board.gotKey(this.crazyOgre.getCol(), this.crazyOgre.getLine())) {
+		this.crazyOgre.setSymbol('$');
+	}else if (!this.crazyOgre.getStun()) {
+		this.crazyOgre.setSymbol('O');
 	}
 
-	// public void OLDprint() {
-	// validateRules();
-	// for (int i = 0; i < 10; i++) {
-	// for (int j = 0; j < 10; j++) {
-	// if (!update(i, j))
-	// System.out.print(board.getMap()[i][j] + " ");
-	// }
-	// System.out.print("\n");
-	// }
-	// if (ended)
-	// System.out.print("PLAYER ONE WINS");
-	// else if (gameover)
-	// System.out.print("GAME OVER\n");
-	// if (level < 2)
-	//
-	// {
-	// if (passed) {
-	//
-	// if (level == 1) {
-	// hero.setLine(1);
-	// hero.setCol(8);
-	// hero.setSymbol('A');
-	// }
-	//
-	// level++;
-	// setLevel();
-	// passed = false;
-	// // novamente print() //break begin;
-	// }
-	// } else if (passed)
-	// ended = true;
-	// }
+	if (this.board.gotKey(this.crazyOgre.getClub().getCol(), this.crazyOgre.getClub().getLine()))
+		this.crazyOgre.getClub().setSymbol('$');
+	else
+		this.crazyOgre.getClub().setSymbol('*');
 
+	if (this.board.foundDoor(this.hero.getLine(), this.hero.getCol()))
+		this.ended = true;
+}
+
+	public void validateRulesLevel3(){
+		//TODO rever o codigo para os 3 guardas
+		this.guard.move();
+
+		if (this.board.gotKey(this.hero.getLine(), this.hero.getCol()))
+			this.board.openDoors();
+
+		if (this.guard.collision(this.hero.getLine(), this.hero.getCol()))
+			this.setGameOver(true);
+
+		if (board.foundDoor(hero.getLine(), hero.getCol()))
+			passed = true;
+	}
+	
+	public void validateRulesLevel4() {
+		for (int i = 0; i < ogres.length; i++) {
+
+			if (!ogres[i].getStun())
+				move(ogres[i]);
+			else
+				ogres[i].incStunCounter();
+
+			CastClub(ogres[i]);
+
+		}
+
+		if (board.gotKey(hero.getLine(), hero.getCol())) {
+			board.openDoors();
+			hero.setSymbol('K');
+		}
+
+		for (int i = 0; i < ogres.length; i++) {
+
+			if (ogres[i].collision(hero.getLine(), hero.getCol()))
+				gameover = true;
+
+			if (ogres[i].stun(hero.getLine(), hero.getCol())) {
+				ogres[i].setSymbol('8');
+				ogres[i].setStun();
+			}
+
+			if (board.gotKey(ogres[i].getCol(), ogres[i].getLine()))
+				ogres[i].setSymbol('$');
+			else if (!ogres[i].getStun())
+				ogres[i].setSymbol('O');
+
+			if (board.gotKey(ogres[i].getClub().getCol(), ogres[i].getClub().getLine()))
+				ogres[i].getClub().setSymbol('$');
+			else
+				ogres[i].getClub().setSymbol('*');
+
+		}
+
+		if (board.foundDoor(hero.getLine(), hero.getCol()))
+			ended = true;
+	}
+
+	
 	public boolean update(int lin, int col) {
 		if (this.hero.getCol() == col && this.hero.getLine() == lin) {
 			System.out.print(this.hero.getSymbol() + " ");
@@ -431,24 +404,24 @@ public class Game {
 
 	public void checkLevel() {
 		
-		if ( this.level == 1 ) {
+		//if ( this.level == 1 ) 
 			// System.out.println("<2");
 			if (this.passed) {
 				// System.out.println("passed");
 
-				if (level == 1) {
+				if (this.level == 1) {
 					// System.out.println("level1");
-					hero.setLine(7);
-					hero.setCol(1);
-					hero.setSymbol('H');
+					this.hero.setLine(7);
+					this.hero.setCol(1);
+					this.hero.setSymbol('H');
 				}
 				// System.out.println("++");
-				level++;
-				setLevel();
-				passed = false;
-				print();
+				this.level++;
+				this.setLevel();
+				this.passed = false;
+				this.print();
 			}
-		} else if (passed)
+		if (this.passed)
 			this.ended = true;
 	
 	}
@@ -458,7 +431,7 @@ public class Game {
 	 */
 
 	public void display() {
-		print();
+		this.print();
 
 		if (this.ended) {
 			System.out.print("PLAYER ONE WINS");
